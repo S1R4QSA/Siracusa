df60ffb953fd4b01d3c47db663db1c57e2bb9160
josE4roldan <josalbacet@gmail.com>
2019-01-06 20:08:39 +0100

    Main modules
    Add main

---
+++ code/main.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Parse the arguments.')
+parser.add_argument('-dn','--domain_name',help='Define the domain name')
+
+
+//ArgumentParser
+
+//ExecuteOptions
+
+//GetInfo
+
+/ShowInfo
