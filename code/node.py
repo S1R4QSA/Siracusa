cb87fa2c67f1192c94ca3f84b1f3182ecaf7b044
PPZeta <pmartinezch@uoc.edu>
2019-01-09 19:37:54 +0100

    First node.py

--- code/node.py
+++ code/node.py
@@ -1,7 +1,10 @@
 from funtionsUtils import checkIP
 
 class Node:
-    data_node: dict = {'hello': 0, 'world': 1}
+    # {'ip':['192.168.1.108','10.16.48.1'], 'dn':['example.com', 'example2.com']}
+    data_node: dict = {'ip':['192.168.1.108','10.16.48.1'], 'dn':['example.com', 'example2.com']}
+    # children: [Node]
+    # parent: Node
 
     def __init__(self):
         pass
@@ -10,13 +13,20 @@ class Node:
         return {k: v for k, v in self.data_node.items() if k in getters}
         
     def set(self, setters: dict) -> bool:
-        self.data_node = {k: v for k, v in self.data_node.items() }
+        for key, value in setters.items():
+            if key in self.data_node:
+                valToIntru = list(filter(lambda x : not x in self.data_node[key], value))
+                self.data_node[key] = self.data_node[key] + valToIntru
+            else:
+                self.data_node[key] = value
         return True
 
     def to_dict(self) -> dict:
         return self.data_node
 
-nodea = Node()
-print (nodea.to_dict())
+# nodea = Node()
+# print (nodea.to_dict())
 
-print (nodea.get(['world']))
+# nodea.set({'ip':['BLABLABLA','10.16.48.1']})
+
+# print (nodea.to_dict())
